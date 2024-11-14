ef9534a1879658d802ef6c6361c18367443b120e
Hafsa Abdalgader <hafos123abdalgader@gmail.com>
2024-11-14 14:20:59 +0100

    todo-app

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
 
 let inputBox = document.getElementById("input-box");
 let todoLista = document.getElementById("todo-lista");
-let button = document.querySelector(".submit");
+let button = document.getElementById("submit-button");
 
 // Array för att hålla varor
+
 let arrayOfTasks = [];
 
  // Lägg till function för button
-  button.onclick = function addTask() {
+  button.onclick = function () {
     if(inputBox.value !== "") { 
       let task = inputBox.value; // Lägger till varor
    arrayOfTasks.push(task);  // Lägg till varar i array
    inputBox.value = ""; // Rensa input-fältet
    displayTasks(); // Visa uppdaterad lista
-   addDataToLocalStorege(arrayOfTasks);  //Spara i localStorage     
+   addDataToLocalStorage(arrayOfTasks);  //Spara i localStorage     
  }
 };
 
 // Function för att visa varor på skärmen 
 function displayTasks() {
-  todoLista.innerHTML = ""; // Rensa listan först
-
-arrayOfTasks.forEach((taskText, index) => {    //Lopping för array
-  let li = document.createElement('li');
-  li.className = 'task';
-  li.innerHTML = '${taskText}' ;'<input type="checkbox" id="task-${index}" ${taskText}';
- 
+   todoLista.innerHTML = ""; // Rensa listan först
+   arrayOfTasks.forEach((taskText, index) => {    //Lopping för array
+    let li = document.createElement('li');
+    li.className = 'task';
+    li.innerHTML =`${taskText}`;
+  
+  
   // Lägga till ta bort knapp
-  let deletButton = document.createElement('button');
-  deletButton.textContent = ("Ta Bort")
-  deletButton.onclick = function(){
+  let deleteButton = document.createElement('button');
+  deleteButton.textContent = ("Ta Bort")
+  deleteButton.onclick = function() {
     removeTask(index);
   };
 
-  li.appendChild(deletButton);
+  li.appendChild(deleteButton);
   todoLista.appendChild(li);
  });
 }
 
-// Function för att to bort en vara lrån listan 
+// Function för att to bort en vara från listan 
 function removeTask(index) {
   arrayOfTasks.splice(index, 1); // To bort varan från arryen
   displayTasks();  // Uppdatera listan
-  addDataToLocalStorege(arrayOfTasks); // Uppdatera localStorage
+  addDataToLocalStorage(arrayOfTasks); // Uppdatera localStorage
 } 
 
 // Spara varor i localStorage 
-function addDataToLocalStorege(tasks) {
-  window.localStorage.setItem("tasks", JSON.stringify(tasks));
+function addDataToLocalStorage(tasks) {
+  window.localStorage.setItem("task", JSON.stringify(tasks));
 }
  
 // Ladd varor från localStorage vid start
 window.onload = function() {
-let storedTasks = JSON.parse(window.localStorage.getItem("tasks"));
+let storedTasks = JSON.parse(window.localStorage.getItem("task"));
 if (Array.isArray(storedTasks)) {
   arrayOfTasks = storedTasks;
   displayTasks(); // Visa varor
@@ -63,6 +64,6 @@ if (Array.isArray(storedTasks)) {
 function del() {
   arrayOfTasks = [];
   displayTasks();
-  addDataToLocalStorege(arrayOfTasks);
+  addDataToLocalStorage(arrayOfTasks);
 }
  
